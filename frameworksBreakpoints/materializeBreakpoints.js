09f544700b4d512f0e2718703479b461e24b180c
LebCit <lebcit@gmail.com>
2024-01-31 09:17:04 +0200

    Define Materialize breakpoints for CSS grid generation

---
+++ frameworksBreakpoints/materializeBreakpoints.js
@@ -0,0 +1,15 @@
+/**
+ * Materialize breakpoints for CSS grid generation, defining minimum viewport widths and optional labels for different layout variations.
+ *
+ * @type {Object.<string, [number, string]>}
+ * @property {[number, string]} sm - Breakpoint for mobile screens (0px or greater).
+ * @property {[number, string]} md - Breakpoint for tablet screens (600px or greater).
+ * @property {[number, string]} lg - Breakpoint for laptop & desktop screens (992px or greater).
+ * @property {[number, string]} xl - Breakpoint for large desktop screens (1200px or greater).
+ */
+export const materializeBreakpoints = {
+	sm: [0, "Mobile"], // Viewport width is 0px or greater
+	md: [600, "Tablet"], // Viewport width is 600px or greater
+	lg: [992, "Laptop & Desktop"], // Viewport width is 992px or greater
+	xl: [1200, "Large Desktop"], // Viewport width is 1200px or greater
+}
