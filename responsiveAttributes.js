241c2f3d99447c45645cf6879380c194aca7e4d7
LebCit <lebcit@gmail.com>
2024-01-31 09:35:59 +0200

    Import object and functions for generating CSS grid rules

---
+++ responsiveAttributes.js
@@ -0,0 +1,8 @@
+// Import the object that holds predefined breakpoint definitions for different frameworks
+import { predefinedBreakpoints } from "./predefinedBreakpoints.js"
+// Import functions for generating CSS grid rules
+import { generateColumnRules } from "./helpers/generateColumnRules.js"
+import { generateColumnSpanRules } from "./helpers/generateColumnSpanRules.js"
+import { generateRowSpanRules } from "./helpers/generateRowSpanRules.js"
+import { generateOrderRules } from "./helpers/generateOrderRules.js"
+
