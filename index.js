0751db7a394a5e75a799d7ccd363b1c1386cda65
LebCit <lebcit@gmail.com>
2024-01-31 10:22:10 +0200

    Import functions to generate and download CCS grid style sheet based on selected framework

---
+++ index.js
@@ -0,0 +1,5 @@
+// Import the `createResponsiveAttributesFile` function from the `responsiveAttributes.js` file.
+import { createResponsiveAttributesFile } from "./responsiveAttributes.js"
+// Import the `getClickedRow` function from the "getClickedRow.js" file
+import { getClickedRow } from "./helpers/getClickedRow.js"
+
